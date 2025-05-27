faff82fd4f85f3c1496cf04978ce5b6fdbe21cbb
ProToType <prototype585@protonmail.com>
2025-05-27 14:43:47 +0200

    deleted unused models

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ChatAppBackend.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
